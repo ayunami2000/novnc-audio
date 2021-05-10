f3a22702a33d3f4ec9638027c8ebc727d4ebc02a
ayunami2000 <spwilliamsiam@gmail.com>
2021-05-10 19:13:42 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ export VNC=5900
 docker build -t ayunami2000/novnc-audio .
 
 docker rm -f novnc-audio
-docker run -it --name novnc-audio -p $NOVNC_PORT:6080 -p $SEL_PORT:4444 -p $AUDIO_PORT:5000 -p $VNC:5900 -v /dev/shm:/dev/shm --privileged --cap-add=ALL -it -v /dev:/dev -v /lib/modules:/lib/modules ayunami2000/novnc-audio
+docker run --name novnc-audio -p $NOVNC_PORT:6080 -p $SEL_PORT:4444 -p $AUDIO_PORT:5000 -p $VNC:5900 -v /dev/shm:/dev/shm --privileged --cap-add=ALL -it -v /dev:/dev -v /lib/modules:/lib/modules ayunami2000/novnc-audio
